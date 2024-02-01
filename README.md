55fdfdd3db638adaaa60428455543def65fef5ec
Thada Wangthammang <thada.wth@gmail.com>
2024-02-01 15:37:21 +0700

    update github actions

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # minimal-typescript-node-esm-starter
 
+build for M1
 
 ```
-docker build -t spy .
+docker buildx build --platform linux/amd64 -t spy . 
 docker run --rm -p 3333:3333 spy
 ```
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT ?? 3000;
 
 // Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! 2');
 });
 
 // Start the server
