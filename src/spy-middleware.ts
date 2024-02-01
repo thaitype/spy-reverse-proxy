d6d0f2fb10820f3763558de69f63bd079ae33929
Thada Wangthammang <thada.wth@gmail.com>
2024-02-01 19:04:02 +0700

    basic middleware

--- src/spy-middleware.ts
+++ src/spy-middleware.ts
@@ -1,16 +1,19 @@
+import 'dotenv/config';
 import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
+import { schema } from './env.schema.js';
+const env = schema.parse(process.env);
 
 /**
  * Configure proxy middleware
  */
 export const spyMiddleware = createProxyMiddleware({
-  target: process.env.TARGET_URL,
+  target: env.TARGET_URL,
   changeOrigin: true, // for vhosted sites, changes host header to match to target's host
   selfHandleResponse: true, // manually call res.end(); IMPORTANT: res.end() is called internally by responseInterceptor()
   onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
     res.setHeader('Powered-by', 'thaitype/spy-reverse-proxy');
 
-    if (req.url === '/api/User/SignIn') {
+    if (req.url === env.TARGET_PATH) {
       res.statusCode = 500;
     }
 
